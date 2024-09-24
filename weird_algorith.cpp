5cdd6d1e7bed437ca4db5433484d53406d6ace14
Vassu05 <vasanthreddy05122@gmail.com>
2024-09-25 01:00:22 +0530

    weird algorithm

---
+++ weird_algorith.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    long long n; cin >> n;
+    while(n > 1) {
+        cout << n << ' ';
+        if(n & 1) n = n * 3 + 1;
+        else n /= 2;
+    }
+    cout << n << '\n';
+}
